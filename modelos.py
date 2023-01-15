cbd7caa162f47857da2ed5d6df28e025f1b96568
Hygor Rocha <hygor.k92@gmail.com>
2023-01-15 20:54:20 -0300

    aula representação textual de obejetos, aprendemos uma forma de apresentar objeto como uma string

--- modelos.py
+++ modelos.py
@@ -19,24 +19,24 @@ class Programa:
     def nome(self, nome):
         self._nome = nome.title()
 
-    def imprime(self):
-        print(f'{self._nome} - {self.ano} - {self._likes} Likes')
+    def __str__(self):
+        return (f'{self._nome} - {self.ano} - {self._likes} Likes')
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
         
-    def imprime(self):
-        print(f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} Likes')
+    def __str__(self):
+        return (f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} Likes')
         
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
         
-    def imprime(self):
-        print(f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes') 
+    def __str__(self):
+        return (f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes') 
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 vingadores.dar_like()
@@ -47,5 +47,5 @@ atlanta.dar_like()
 filmes_e_series = [vingadores, atlanta]
 
 for programa in filmes_e_series:
-    programa.imprime()
+    print(programa)
     
