cfdee1dfaf0793795026fa215fa948774a7ffca4
Hygor Rocha <hygor.k92@gmail.com>
2023-01-15 21:16:22 -0300

    criando a classe de playlist

--- modelos.py
+++ modelos.py
@@ -36,16 +36,33 @@ class Serie(Programa):
         self.temporadas = temporadas
         
     def __str__(self):
-        return (f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes') 
-
+        return (f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes')
+    
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self.programas = programas
+        
+    def tamanho(self):
+        return(len(self.programas))    
+        
+        
+        
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_like()
 atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
+vingadores.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
 
-filmes_e_series = [vingadores, atlanta]
 
-for programa in filmes_e_series:
+filmes_e_series = [vingadores, atlanta, demolidor, tmep]
+
+playlist_fim_de_semana = Playlist('Playlist fim de semana', filmes_e_series)
+
+for programa in playlist_fim_de_semana.programas:
     print(programa)
     
